03fa09336aaea316c8f80dee1b87578e48e97076
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2024-11-15 11:23:37 -0500

    Report tests

--- src/Iteration2TestsMaina/MarkEvent.java
+++ src/Iteration2TestsMaina/MarkEvent.java
@@ -2,20 +2,7 @@ package Iteration2TestsMaina;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Duration;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,6 +13,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 class MarkEvent {
 
@@ -44,13 +33,13 @@ class MarkEvent {
 	}
 
 		
-	private void loadContentManagementPage() {
+	private void loadEventDetailsPage() {
 		driver.get("http://localhost:3000/");
 
 		driver.manage().window().maximize();
 
 		WebElement inputField = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/input[1]"));
-		String testEmail = "mainaaa.16@gmail.com";
+		String testEmail = "user@gmail.com";
 		inputField.sendKeys(testEmail);
 
 		WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/input[2]"));
@@ -66,9 +55,63 @@ class MarkEvent {
 		eventDetailsBtn.click();
 	}
 	
+	public static String getBaseURL(String fullUrl) {
+        if (fullUrl.contains("/event/")) {
+            return fullUrl.split("/event/")[0] + "/event/";
+        } else {
+            return fullUrl;
+        }
+    }
+	
 	@Test
-	void testAccessContentManagement() {
+	void testAccessEventDetails() {
+		loadEventDetailsPage();
+		String url = driver.getCurrentUrl();
+		String expectedUrl = "http://localhost:3000/event/";
+		String baseURL = getBaseURL(url);
 		
+		assertEquals(expectedUrl, baseURL);		
 	}
+	
+	@Test
+	void testAttendEventCheckboxVisible() {
+		loadEventDetailsPage();
+		
+		WebElement attend = driver.findElement(By.xpath("//*[@id=\"attendEvent\"]"));
+		Boolean attendDisplayed = attend.isDisplayed();
+		assertTrue(attendDisplayed);
+	}
+	
+	@Test
+	void testAttendEventCheckboxClickable() {
+		loadEventDetailsPage();
+		
+		WebElement attend = driver.findElement(By.xpath("//*[@id=\"attendEvent\"]"));
+		attend.click();
+	}
+	
+	@Test
+	void testAttendEventCheckboxIsChecked() {
+		loadEventDetailsPage();
+		
+		WebElement attend = driver.findElement(By.xpath("//*[@id=\"attendEvent\"]"));
+		attend.click();
+		Boolean attendChecked = attend.isSelected();
+		assertTrue(attendChecked);
+	}
+	
+	@Test
+	void testAttendEventCheckboxStaysChecked() {
+	    loadEventDetailsPage();
+	    
+	    WebElement attend = driver.findElement(By.xpath("//*[@id=\"attendEvent\"]"));
+	    attend.click();
+
+	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	    boolean attendChecked = wait.until(ExpectedConditions.elementSelectionStateToBe(By.xpath("//*[@id=\"attendEvent\"]"), true));
+	    
+	    assertTrue(attendChecked);	    
+	}
+
 
 }
