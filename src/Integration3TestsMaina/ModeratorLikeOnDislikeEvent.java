ee0f99ef5bbf4acc967ffe43ef9c2ef05c7be97a
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2024-11-19 19:55:08 -0500

    New tests

--- src/Integration3TestsMaina/ModeratorLikeOnDislikeEvent.java
+++ src/Integration3TestsMaina/ModeratorLikeOnDislikeEvent.java
@@ -67,9 +67,116 @@ class ModeratorLikeOnDislikeEvent {
 		
 		Boolean buttonDisplayed = likeButton.isDisplayed();
 		
-		assertTrue(buttonDisplayed);
+		assertTrue(buttonDisplayed);	
+	}
+	
+	@Test
+	void testDislikeButtonDisplayed() {
+		loadHomePage();
 		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement dislikeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[2]/button[2]")));
 		
+		Boolean buttonDisplayed = dislikeButton.isDisplayed();
+		
+		assertTrue(buttonDisplayed);	
+	}
+	
+	@Test
+	void testLikeButtonEnabled() {
+		loadHomePage();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement likeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[2]/button[1]")));
+		
+		Boolean buttonEnabled = likeButton.isEnabled();
+		
+		assertTrue(buttonEnabled);	
+	}
+	
+	@Test
+	void testDislikeButtonEnabled() {
+		loadHomePage();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement dislikeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[2]/button[2]")));
+		
+		Boolean buttonEnabled = dislikeButton.isEnabled();
+		
+		assertTrue(buttonEnabled);	
+	}
+	
+	@Test
+	void testLikeButtonText() {
+		loadHomePage();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement likeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[2]/button[1]")));
+		
+		String actualText = likeButton.getText();
+		
+		String expectedText = "Like";
+		
+		assertEquals(expectedText, actualText);
+	}
+	
+	@Test
+	void testdislikeButtonText() {
+		loadHomePage();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		WebElement dislikeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[2]/button[2]")));
+		
+		String actualText = dislikeButton.getText();
+		
+		String expectedText = "Dislike";
+		
+		assertEquals(expectedText, actualText);
+	}
+
+	@Test
+	void testLikeButtonCount() {
+		loadHomePage();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));		
+		WebElement likeCount = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[1]/p[1]")));
+		String likeCountBefore = likeCount.getText();
+		System.out.println(likeCountBefore);
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement likeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[2]/div[2]/button[1]")));
+		likeButton.click();
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		String likeCountAfter = likeCount.getText();	
+		System.out.println(likeCountAfter);
+		
+		assertEquals(likeCountBefore, likeCountAfter);
+	}
+	
+	@Test
+	void testDislikeButtonCount() {
+		loadHomePage();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));		
+		WebElement dislikeCount = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[2]/div[1]/p[2]")));
+		String dislikeCountBefore = dislikeCount.getText();
+		System.out.println(dislikeCountBefore);
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement dislikeButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[4]/div[2]/div/div[1]/div[2]/button[2]")));
+		dislikeButton.click();
+		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		String dislikeCountAfter = dislikeCount.getText();	
+		System.out.println(dislikeCountAfter);
+		
+		assertEquals(dislikeCountBefore, dislikeCountAfter);
 	}
 
 }
