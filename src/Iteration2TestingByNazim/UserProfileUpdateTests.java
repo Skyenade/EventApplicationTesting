07a643f8289451d426241238e0e87d8af0bac974
MahammedNazim25 <148381801+MahammedNazim25@users.noreply.github.com>
2024-11-27 07:25:03 -0500

    updates

--- src/Iteration2TestingByNazim/UserProfileUpdateTests.java
+++ src/Iteration2TestingByNazim/UserProfileUpdateTests.java
@@ -1,21 +1,26 @@
 package Iteration2TestingByNazim;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserProfileUpdateTests {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("http://localhost:3000");
     }
 
     @AfterEach
@@ -25,35 +30,75 @@ public class UserProfileUpdateTests {
         }
     }
 
-    private void loadLoginPage() {
-        driver.get("http://localhost:3000");
-    }
-
-    private void login(String email, String password) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
+    public void login(String email, String password) {
         WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/form/input[1]")));
         WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/form/input[2]")));
-        
+
         emailField.sendKeys(email);
         passwordField.sendKeys(password);
-        
+
         WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/form/button[1]"));
         loginButton.click();
     }
 
-    @Test
-    void testUpdateUserProfile() {
-        loadLoginPage();
-        login("test01@gmail.com", "123456");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
+    private void navigateToUserProfile() {
         WebElement profileButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='userprofile-button']")));
         profileButton.click();
+    }
+
+    @Test
+    void testProfileButtonPresence() {
+        login("test01@gmail.com", "123456");
+        WebElement profileButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='userprofile-button']")));
+        assertNotNull(profileButton, "Profile button should be present.");
+    }
+
+    @Test
+    void testProfilePageHeaderPresence() {
+        login("test01@gmail.com", "123456");
+        navigateToUserProfile();
 
         WebElement profilePageHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text()='User Profile']")));
-        assertTrue(profilePageHeader.isDisplayed());
+        assertTrue(profilePageHeader.isDisplayed(), "User Profile header should be displayed.");
+    }
+
+    @Test
+    void testNameFieldEditable() {
+        login("test01@gmail.com", "123456");
+        navigateToUserProfile();
+
+        WebElement nameField = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/input"));
+        nameField.clear();
+        nameField.sendKeys("New User Name");
+
+        assertEquals("New User Name", nameField.getAttribute("value"), "Name field should be updated.");
+    }
+
+    @Test
+    void testBioFieldEditable() {
+        login("test01@gmail.com", "123456");
+        navigateToUserProfile();
+
+        WebElement bioField = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/textarea"));
+        bioField.clear();
+        bioField.sendKeys("This is my updated bio.");
+
+        assertEquals("This is my updated bio.", bioField.getAttribute("value"), "Bio field should be updated.");
+    }
+
+    @Test
+    void testSaveButtonPresence() {
+        login("test01@gmail.com", "123456");
+        navigateToUserProfile();
+
+        WebElement saveButton = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/button[2]"));
+        assertNotNull(saveButton, "Save button should be present.");
+    }
+
+    @Test
+    void testSaveProfileSuccessMessage() {
+        login("test01@gmail.com", "123456");
+        navigateToUserProfile();
 
         WebElement nameField = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/input"));
         nameField.clear();
@@ -67,43 +112,22 @@ public class UserProfileUpdateTests {
         saveButton.click();
 
         WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='Profile updated successfully']")));
-        assertTrue(successMessage.isDisplayed());
-
-        WebElement updatedName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/p[1]")));
-        assertEquals("New User Name", updatedName.getText());
-        assertEquals("This is my updated bio.", bioField.getAttribute("value"));
+        assertTrue(successMessage.isDisplayed(), "Success message should be displayed after saving profile.");
     }
 
     @Test
-    void testUpdateUserProfileWithSpecialCharsAndNumbers() {
-        loadLoginPage();
+    void testUpdatedNameDisplayed() {
         login("test01@gmail.com", "123456");
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        WebElement profileButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='userprofile-button']")));
-        profileButton.click();
-
-        WebElement profilePageHeader = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text()='User Profile']")));
-        assertTrue(profilePageHeader.isDisplayed());
+        navigateToUserProfile();
 
         WebElement nameField = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/input"));
         nameField.clear();
-        nameField.sendKeys("User123!@#");
-
-        WebElement bioField = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/textarea"));
-        bioField.clear();
-        bioField.sendKeys("This is my bio with special characters !@#$%^&*()123.");
+        nameField.sendKeys("New User Name");
 
         WebElement saveButton = driver.findElement(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/button[2]"));
         saveButton.click();
 
-        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='Profile updated successfully']")));
-        assertTrue(successMessage.isDisplayed());
-
         WebElement updatedName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='root']/div/div/div[2]/div[1]/p[1]")));
-        assertEquals("User123!@#", updatedName.getText());
-
-        assertEquals("This is my bio with special characters !@#$%^&*()123.", bioField.getAttribute("value"));
+        assertEquals("New User Name", updatedName.getText(), "Updated name should be displayed on the profile page.");
     }
 }
--- src/Iteration3Nazim/UserManagementTests.java
+++ src/Iteration3Nazim/UserManagementTests.java
@@ -1,102 +1,101 @@
 package Iteration3Nazim;
 
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class UserManagementTests {
 
-    WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
 
- 
-    public void loadAdminDashboard() {
+    @BeforeEach
+    public void setup() {
         
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void teardown() {
+        
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @BeforeEach
+    public void loadAdminDashboard() {
         driver.get("http://localhost:3000");
 
-        
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement inputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/form/input[1]")));
         inputField.sendKeys("admin@gmail.com");
 
-        
         WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/form/input[2]")));
         passwordField.sendKeys("admin1234");
 
-       
         WebElement submitBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/form/button[1]")));
         submitBtn.click();
 
-    
         wait.until(ExpectedConditions.urlContains("/AdminHome"));
 
-      
         WebElement adminDashboardLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[3]/div[3]/div[1]/h4/a")));
         adminDashboardLink.click();
 
-       
         wait.until(ExpectedConditions.urlContains("/AdminDashboard"));
 
-       
         WebElement userManagementBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div[1]/a")));
         userManagementBtn.click();
 
-        
         wait.until(ExpectedConditions.urlContains("/UserManagement"));
     }
 
-    
-    public void testEditProfileButtonPresence() {
-        loadAdminDashboard(); 
-
+    @Test
+     void testEditProfileButtonPresence() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement editProfileBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[4]/button[1]")));
-        System.out.println("Edit Profile button is present: " + (editProfileBtn.isDisplayed() ? "Yes" : "No"));
+        Assertions.assertTrue(editProfileBtn.isDisplayed(), "Edit Profile button should be displayed");
     }
 
-    public void testSuspendAccountButtonPresence() {
-        loadAdminDashboard(); 
-
+    @Test
+     void testSuspendAccountButtonPresence() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement suspendAccountBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[4]/button[2]")));
-        System.out.println("Suspend Account button is present: " + (suspendAccountBtn.isDisplayed() ? "Yes" : "No"));
+        Assertions.assertTrue(suspendAccountBtn.isDisplayed(), "Suspend Account button should be displayed");
     }
 
-    public void testDeleteAccountButtonPresence() {
-        loadAdminDashboard(); 
-
+    @Test
+     void testDeleteAccountButtonPresence() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement deleteAccountBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[4]/button[3]")));
-        System.out.println("Delete Account button is present: " + (deleteAccountBtn.isDisplayed() ? "Yes" : "No"));
+        Assertions.assertTrue(deleteAccountBtn.isDisplayed(), "Delete Account button should be displayed");
     }
 
-    
-    public void testRestoreButtonPresence() {
-        loadAdminDashboard(); 
-
+    @Test
+     void testRestoreButtonPresence() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement restoreBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[4]/button[4]")));
-        System.out.println("Restore button is present: " + (restoreBtn.isDisplayed() ? "Yes" : "No"));
+        Assertions.assertTrue(restoreBtn.isDisplayed(), "Restore button should be displayed");
     }
 
-
-    public void testDeleteProfileButtonPresence() {
-        loadAdminDashboard(); 
-
+    @Test
+     void testDeleteProfileButtonPresence() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement deleteProfileBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[4]/button[5]")));
-        System.out.println("Delete Profile button is present: " + (deleteProfileBtn.isDisplayed() ? "Yes" : "No"));
+        Assertions.assertTrue(deleteProfileBtn.isDisplayed(), "Delete Profile button should be displayed");
     }
 
-  
-    public void testRestoreProfileButtonPresence() {
-        loadAdminDashboard();
-
+    @Test
+     void testRestoreProfileButtonPresence() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement restoreProfileBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/table/tbody/tr[1]/td[4]/button[6]")));
-        System.out.println("Restore Profile button is present: " + (restoreProfileBtn.isDisplayed() ? "Yes" : "No"));
+        Assertions.assertTrue(restoreProfileBtn.isDisplayed(), "Restore Profile button should be displayed");
     }
 }
